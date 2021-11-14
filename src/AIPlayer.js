8f8b90844545de6f3f7af426714c55f778cc9a10
hiphurraa <61519166+hiphurraa@users.noreply.github.com>
2021-11-14 23:55:08 +0200

    Improvements to AI

--- src/AIPlayer.js
+++ src/AIPlayer.js
@@ -12,6 +12,62 @@ class AIPlayer {
         return Math.floor(Math.random() * (max - min + 1) + min)
     }
 
+    checkForWildCards(up, upRight, right, rightDown, down, downLeft, left, leftUp) {
+        if (up.substring(0, 3) == "PPe" && down.substring(0, 2) == "Pe") return 99;
+        if (upRight.substring(0, 3) == "PPe" && downLeft.substring(0, 2) == "Pe") return 99;
+        if (right.substring(0, 3) == "PPe" && left.substring(0, 2) == "Pe") return 99;
+        if (rightDown.substring(0, 3) == "PPe" && leftUp.substring(0, 2) == "Pe") return 99;
+        if (down.substring(0, 3) == "PPe" && up.substring(0, 2) == "Pe") return 99;
+        if (downLeft.substring(0, 3) == "PPe" && upRight.substring(0, 2) == "Pe") return 99;
+        if (left.substring(0, 3) == "PPe" && right.substring(0, 2) == "Pe") return 99;
+        if (leftUp.substring(0, 3) == "PPe" && rightDown.substring(0, 2) == "Pe") return 99;
+
+        if (up.substring(0, 1) == "e" && down.substring(0, 4) == "PPPe") return 99;
+        if (upRight.substring(0, 1) == "e" && downLeft.substring(0, 4) == "PPPe") return 99;
+        if (right.substring(0, 1) == "e" && left.substring(0, 4) == "PPPe") return 99;
+        if (rightDown.substring(0, 1) == "e" && leftUp.substring(0, 4) == "PPPe") return 99;
+        if (down.substring(0, 1) == "e" && up.substring(0, 4) == "PPPe") return 99;
+        if (downLeft.substring(0, 1) == "e" && upRight.substring(0, 4) == "PPPe") return 99;
+        if (left.substring(0, 1) == "e" && right.substring(0, 4) == "PPPe") return 99;
+        if (leftUp.substring(0, 1) == "e" && rightDown.substring(0, 4) == "PPPe") return 99;
+
+        if (up.substring(0,1) == "P" && down.substring(0,3) == "PPP") return 999;
+        if (upRight.substring(0, 1) == "P" && downLeft.substring(0, 3) == "PPP") return 999;
+        if (right.substring(0, 1) == "P" && left.substring(0, 3) == "PPP") return 999;
+        if (rightDown.substring(0, 1) == "P" && leftUp.substring(0, 3) == "PPP") return 999;
+        if (down.substring(0, 1) == "P" && up.substring(0, 3) == "PPP") return 999;
+        if (downLeft.substring(0, 1) == "P" && upRight.substring(0, 3) == "PPP") return 999;
+        if (left.substring(0, 1) == "P" && right.substring(0, 3) == "PPP") return 999;
+        if (leftUp.substring(0, 1) == "P" && rightDown.substring(0, 3) == "PPP") return 999;
+
+        if (up.substring(0,2) == "PP" && down.substring(0,2) == "PP") return 999;
+        if (upRight.substring(0, 2) == "PP" && downLeft.substring(0, 2) == "PP") return 999;
+        if (right.substring(0, 2) == "PP" && left.substring(0, 2) == "PP") return 999;
+        if (rightDown.substring(0, 2) == "PP" && leftUp.substring(0, 2) == "PP") return 999;
+        if (down.substring(0, 2) == "PP" && up.substring(0, 2) == "PP") return 999;
+        if (downLeft.substring(0, 2) == "PP" && upRight.substring(0, 2) == "PP") return 999;
+        if (left.substring(0, 2) == "PP" && right.substring(0, 2) == "PP") return 999;
+        if (leftUp.substring(0, 2) == "PP" && rightDown.substring(0, 2) == "PP") return 999;
+
+        if (up.substring(0, 1) == "A" && down.substring(0, 3) == "AAA") return 999999;
+        if (upRight.substring(0, 1) == "A" && downLeft.substring(0, 3) == "AAA") return 999999;
+        if (right.substring(0, 1) == "A" && left.substring(0, 3) == "AAA") return 999999;
+        if (rightDown.substring(0, 1) == "A" && leftUp.substring(0, 3) == "AAA") return 999999;
+        if (down.substring(0, 1) == "A" && up.substring(0, 3) == "AAA") return 999999;
+        if (downLeft.substring(0, 1) == "A" && upRight.substring(0, 3) == "AAA") return 999999;
+        if (left.substring(0, 1) == "A" && right.substring(0, 3) == "AAA") return 999999;
+        if (leftUp.substring(0, 1) == "A" && rightDown.substring(0, 3) == "AAA") return 999999;
+
+        if (up.substring(0,2) == "AA" && down.substring(0,2) == "AA") return 999999;
+        if (upRight.substring(0, 2) == "AA" && downLeft.substring(0, 2) == "AA") return 999999;
+        if (right.substring(0, 2) == "AA" && left.substring(0, 2) == "AA") return 999999;
+        if (rightDown.substring(0, 2) == "AA" && leftUp.substring(0, 2) == "AA") return 999999;
+        if (down.substring(0, 2) == "AA" && up.substring(0, 2) == "AA") return 999999;
+        if (downLeft.substring(0, 2) == "AA" && upRight.substring(0, 2) == "AA") return 999999;
+        if (left.substring(0, 2) == "AA" && right.substring(0, 2) == "AA") return 999999;
+        if (leftUp.substring(0, 2) == "AA" && rightDown.substring(0, 2) == "AA") return 999999;
+    }
+
     calculatePriority(gameGrid, x, y) {
         let up = this.scanner.scanUp(gameGrid, x, y);
         let upRight = this.scanner.scanUpRight(gameGrid, x, y);
@@ -24,24 +80,14 @@ class AIPlayer {
 
         let priority = priorities[up] + priorities[upRight] + priorities[right] + priorities[rightDown] + priorities[down] + 
             priorities[downLeft] + priorities[left] + priorities[leftUp];
-            
+
+        let wildCardPriority = this.checkForWildCards(up, upRight, right, rightDown, down, downLeft, left, leftUp);
+        
+        if (wildCardPriority > priority) return wildCardPriority;
         return priority;
     }
 
     play(gameGrid) {
-        // Randomize the first move
-        if (this.firstMove){
-            let x, y;
-            do {
-                x = this.randomIntFromInterval(4, 9);
-                y = this.randomIntFromInterval(4, 9);
-            } while (gameGrid[y][x] != null)
-            console.log(x + ', ' + y);
-            gameGrid[y][x] = "A";
-            this.firstMove = false;
-            return gameGrid;
-        }
-
         var topPriority = { 0: [0, 0] };
 
         for (let y = 0; y < gameGrid.length; y++) {
